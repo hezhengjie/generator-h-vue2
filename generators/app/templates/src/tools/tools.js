7142b6c652e0f61060ec42f021571e69cbeefadb
hugh <hezhengjie@meituan.com>
2017-05-27 17:03:10 +0800

    webpack 迁移至2.0

--- generators/app/templates/src/tools/tools.js
+++ generators/app/templates/src/tools/tools.js
@@ -1,8 +1,7 @@
 /*
    封装一些常用方法
  */
-import UA from '@dp/util-m-ua';
-import KNB from '@dp/knb';
+
 
 let Tool = {
     /*
@@ -13,51 +12,6 @@ let Tool = {
         let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
         let results = regex.exec(location.search);
         return results === null ? '' : decodeURIComponent(results[1])
-    },
-    /*
-     判断是否在App内
-     */
-    isInApp(){
-        return UA().type==="dpapp"||UA().type==="mtgroup";
-    },
-
-    /*
-     返回
-     */
-    back(){
-        if(history.length>1){
-            history.back();
-        }
-        else{
-            KNB.closeWebview({});
-        }
-    },
-    /*
-      跳转（app和h5）
-     */
-    jump(url,notitlebar){
-        if(UA().type==="dpapp"||UA().type==="mtgroup"){
-            KNB.openWebview({
-                url:url,
-                qs: {
-                    notitlebar:notitlebar
-                }
-            });
-        }
-        else {
-            location.href=url
-        }
-    },
-
-
-    /*
-      灵犀打点(4.0)
-     */
-    lxView(bid,lab){
-        LXAnalytics('moduleView', bid,lab)
-    },
-    lxClick(bid,lab){
-        LXAnalytics('moduleClick',bid, lab);
     }
 };
 
